ead458c4d99511118127d6977dcb511d2a56085d
Filipe Avanzzo <avanzzodev@gmail.com>
2023-11-02 14:51:43 -0300

    handle submit logic and listing itens feature done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textareaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
 const formEl = document.querySelector(".form");
+const listEl = document.querySelector(".feedbacks");
 
 const handleInput = () => {
   const maxCh = 150;
@@ -21,7 +22,36 @@ const handleSubmit = (e) => {
     setTimeout(() => {
       formEl.classList.remove("form--invalid");
     }, 2000);
+
+    textareaEl.focus();
+
+    return;
   }
+
+  const hashtag = text.split(" ").find((e) => e.includes("#"));
+  const companyName = hashtag.replace("#", "");
+  const companyFirstLetter = companyName[0].toUpperCase();
+  const upVoteCount = 0;
+  const daysAgo = 0;
+
+  const feedbackItemHTML = `
+    <li class="feedback">
+    <button class="upvote">
+        <i class="fa-solid fa-caret-up upvote__icon"></i>
+        <span class="upvote__count">${upVoteCount}</span>
+    </button>
+    <section class="feedback__badge">
+        <p class="feedback__letter">${companyFirstLetter}</p>
+    </section>
+    <div class="feedback__content">
+        <p class="feedback__company">${companyName}</p>
+        <p class="feedback__text">${text}</p>
+    </div>
+    <p class="feedback__date">${daysAgo === 0 ? "New" : daysAgo + "d"}</p>
+  </li>
+  `;
+
+  listEl.insertAdjacentHTML("beforeend", feedbackItemHTML);
 };
 
 textareaEl.addEventListener("input", handleInput);
